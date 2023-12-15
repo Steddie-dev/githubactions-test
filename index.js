ef8dcf9fb78c31bd0c8d6bfb7b2f264da2f4a11d
Steddie-dev <143703038+Steddie-dev@users.noreply.github.com>
2023-12-15 14:08:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', (req, res) => {
   res
     .status(200)
-    .send('Hello server is running,Lets see how it goes')
+    .send('Hello server is runnings')
     .end();
 });
  
